9289fc13772f5de8db28a45192acdcbeaedab168
Yoonkyo Jung <48821642+Yoonkyo@users.noreply.github.com>
2021-12-19 23:37:36 +0900

    fix time limit exceeded problem

--- leetcode/leetcode_211212_B.py
+++ leetcode/leetcode_211212_B.py
@@ -3,9 +3,12 @@ class Solution(object):
         length = len(nums)
         result = 0
         for i in range(length):
-            for j in range(i, length):
-                temp_list = nums[i:j+1]
-                result += max(temp_list)-min(temp_list)
+            max_num = nums[i]
+            min_num = nums[i]
+            for j in range(i+1, length):
+                max_num = max(max_num, nums[j])
+                min_num = min(min_num, nums[j])
+                result += max_num - min_num
         return result
         
         """
