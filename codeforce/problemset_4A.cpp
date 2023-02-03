1d5e1e9a9a72ac6010893fb4c035d66ed6081661
Yoonkyo Jung <48821642+Yoonkyo@users.noreply.github.com>
2023-02-03 18:28:31 +0900

    Create problemset_4A.cpp

---
+++ codeforce/problemset_4A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int w;
+    cin >> w;
+    if (w < 4){
+        cout<<"NO";
+    }
+    else {
+        if (w%2 == 1){
+            cout<<"NO";
+        }
+        else cout<<"YES";
+    }
+
+    return 0;
+}
